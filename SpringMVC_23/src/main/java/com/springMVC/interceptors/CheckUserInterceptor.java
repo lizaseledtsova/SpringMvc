44742e8fd7b77728f73dc53789b0eb22a6103ce0
liza <liza@DESKTOP-M4PO6CG>
2020-07-24 21:29:46 +0300

    SpirngMVC

---
+++ SpringMVC_23/src/main/java/com/springMVC/interceptors/CheckUserInterceptor.java
@@ -0,0 +1,25 @@
+package com.springMVC.interceptors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.springMVC.objects.User;
+
+public class CheckUserInterceptor extends HandlerInterceptorAdapter { // implements HandlerInterceptor{
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		if (request.getRequestURI().contains("check-user")) {
+
+			User user = (User) modelAndView.getModel().get("user");
+			if (user == null || !user.isAdmin()) {
+				response.sendRedirect(request.getContextPath() + "/failed");
+			}
+
+		}
+	}
+
+}
---
+++ SpringMVC_23/src/main/java/com/springMVC/pdf/PDFBuilder.java
@@ -0,0 +1,25 @@
+package com.springMVC.pdf;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.springMVC.objects.User;
+
+public class PDFBuilder extends AbstractPdfView {
+
+	@Override
+	protected void buildPdfDocument(Map<String, Object> model, Document doc, PdfWriter writer, HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		User user = (User) model.get("user");
+
+		doc.add(new Paragraph(user.getName()));
+		doc.add(new Paragraph(user.getPassword()));
+
+	}
+
+}
